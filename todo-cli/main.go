d9a1c8ca6e977de7ef814a1e0dfae3e05b69fa6b
Andrew Arochukwu <andrewarochukwu@gmail.com>
2024-09-01 20:11:59 -0400

    almost done proj 1

--- todo-cli/main.go
+++ todo-cli/main.go
@@ -6,36 +6,106 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"text/tabwriter"
 )
 
-func main() {
-	file, err := os.OpenFile("data.csv", os.O_APPEND, 0644)
+func add(file string) {
+	fmt.Printf("add function %s", file)
+}
+
+func delete(file string) {
+	fmt.Printf("delete function %s", file)
+}
+
+func complete(file string) {
+	fmt.Printf("complete function %s", file)
+}
+
+func list(file string) {
+	w := tabwriter.NewWriter(os.Stdout, 0, 2, 3, ' ', 0)
+
+	filer, err := os.Open(file)
 	if err != nil {
 		panic(err)
 	}
+	defer filer.Close()
 
-	defer file.Close()
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-
-	row := []string{"2", "finish go project", "2024-09-01", "No"}
-	err = writer.Write(row)
-	if err != nil {
-		panic(err)
-	}
-
-	reader := csv.NewReader(file)
+	reader := csv.NewReader(filer)
 	reader.FieldsPerRecord = -1
 	data, err := reader.ReadAll()
 	if err != nil {
 		panic(err)
 	}
 
-	for _, row := range data {
-		for _, col := range row {
-			fmt.Printf("%s", col)
+	for pos, row := range data {
+		if pos > 0 && row[3] == "No" {
+			var str string = ""
+			for _, col := range row {
+				str += col + "\t"
+			}
+			fmt.Fprintln(w, str)
 		}
-		fmt.Println()
+	}
+	w.Flush()
+}
+
+func list_a(file string) {
+	w := tabwriter.NewWriter(os.Stdout, 0, 2, 3, ' ', 0)
+
+	filer, err := os.Open(file)
+	if err != nil {
+		panic(err)
+	}
+	defer filer.Close()
+
+	reader := csv.NewReader(filer)
+	reader.FieldsPerRecord = -1
+	data, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
 	}
 
+	for pos, row := range data {
+		if pos > 0 {
+			var str string = ""
+			for _, col := range row {
+				str += col + "\t"
+			}
+			fmt.Fprintln(w, str)
+		}
+	}
+	w.Flush()
+}
+
+func main() {
+	var option int
+	var file string
+	fmt.Println("Please enter your list file")
+	fmt.Scan(&file)
+	fmt.Println("Pick what you want to do")
+	fmt.Println("1. list (lists none completed items)")
+	fmt.Println("2. list -a (lists items on the list)")
+	fmt.Println("3. add (<task details - add later in cobra> adds item to list)")
+	fmt.Println("4. complete (<task id - add later in cobra> marks task as complete)")
+	fmt.Println("5. delete (<task id - add later in cobra> deletes task from list)")
+	fmt.Println("6. end")
+	fmt.Scan(&option)
+	for {
+		if option == 6 {
+			break
+		}
+		switch option {
+		case 1:
+			list(file)
+		case 2:
+			list_a(file)
+		case 3:
+			add(file)
+		case 4:
+			complete(file)
+		case 5:
+			delete(file)
+		}
+		fmt.Scan(&option)
+	}
 }
